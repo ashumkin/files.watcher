818118f6d56af3eed4a67bf236fcc4d833dd1b34
Alexey Shumkin <Alex.Crezoff@gmail.com>
2013-06-14 12:23:53 +0400

    improved: helpers: time string converter for ffmpeg
    This helper converts "hh:mm:ss - hh:mm:ss" string
    to "-ss <start_time> -t <duration>" options for ffmpeg

---
+++ helpers/ffmpeg_time.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import sys, re, time, datetime
+
+class Time(object):
+    def __init__(self, seconds):
+        self.seconds = seconds
+        self.__timeformat = '%H:%M:%S'
+
+    @property
+    def string(self):
+        return time.strftime(self.__timeformat, time.gmtime(self.seconds))
+
+    @string.setter
+    def string(self, value):
+        x = time.strptime(value, self.__timeformat)
+        self.seconds = datetime.timedelta(hours = x.tm_hour, minutes = x.tm_min, seconds = x.tm_sec).total_seconds()
+
+class TimeParser(object):
+    def __init__(self):
+        self.time_s = Time(0)
+        self.time_f = Time(0)
+
+    @property
+    def time(self):
+        return '%s-%s' % (self.time_s.string, self.time_f.string)
+
+    @time.setter
+    def time(self, value):
+        self.__parse_time(value)
+
+    def __parse_time(self, value):
+        m = re.search('(\d+:\d+:\d+)\s*-\s*(\d+:\d+:\d+)', value)
+        if m is None:
+            raise ValueError('`%s` is not a time range' % value)
+        self.time_s.string = m.group(1)
+        self.time_f.string = m.group(2)
+
+    @property
+    def __duration(self):
+        return self.time_f.seconds - self.time_s.seconds
+
+    def forffmpeg(self):
+        return '-ss %s -t %d' % (self.time_s.string, self.__duration)
+
+    def read_from_stdin(self):
+        for line in sys.stdin:
+            self.time = line
+            # read first line only
+            break
+
+if __name__ == '__main__':
+    tp = TimeParser()
+    tp.read_from_stdin()
+    print tp.forffmpeg()
---
+++ test/test-TimeParser.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import os, sys, inspect
+import unittest
+
+# use this if you want to include modules from a subfolder
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0], "../helpers")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+import ffmpeg_time
+
+class TestTime(unittest.TestCase):
+    def setUp(self):
+        self.time = ffmpeg_time.Time(124)
+
+    def test_seconds(self):
+        self.assertEqual(124, self.time.seconds)
+
+    def test_string(self):
+        self.assertEqual('00:02:04', self.time.string)
+        self.time.string = '0:3:5'
+        self.assertEqual(185, self.time.seconds)
+        self.assertEqual('00:03:05', self.time.string)
+
+class TestTimeParser(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = ffmpeg_time.TimeParser()
+        self.parser.time = '0:0:5-0:1:43'
+
+    def test_time(self):
+        self.assertEqual('00:00:05-00:01:43', self.parser.time)
+        self.parser.time = '0:0:6 -   2:0:48'
+        self.assertEqual('00:00:06-02:00:48', self.parser.time)
+
+    def test_forffmpeg(self):
+        self.assertEqual('-ss 00:00:05 -t 98', self.parser.forffmpeg())
+
+    def test_incorrect_time(self):
+        with self.assertRaises(ValueError):
+            self.parser.time = '0::0:5'
+
+if __name__ == '__main__':
+    unittest.main()
